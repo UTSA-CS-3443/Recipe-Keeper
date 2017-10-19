085149d2306d34e2bed40d78bdf81b83570101fb
Hoa Pham <rzz934@my.utsa.edu>
2017-10-19 15:05:10 -0500

    Create NewInterFace and NewController

---
+++ src/control/NewController.java
@@ -0,0 +1,43 @@
+package control;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+
+public class NewController implements Initializable{
+	@FXML 
+	BorderPane motherPane = new BorderPane();
+	
+	private void addIngredient() {
+		
+	}
+	
+	private void delIngredient() {
+		
+	}
+	
+	private void addCategory() {
+		
+	}
+	
+	private void delCategory() {
+		
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+}
